8b40e6516af21128ae54fd2e24b720ca2359d325
luthfi <kaplingtumbal@gmail.com>
2025-08-12 06:07:15 +0700

    auth for mentor

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,37 @@
 var jwt = require("jsonwebtoken")
 var exports = module.exports = {}
+var supabase = require("../database/supabase").client
 
 exports.auth = async (req, res, next) => {
     try {
         const user = jwt.verify(req.cookies.access_token, process.env.SUPABASE_JWT_KEY)
         req.user = user.user_metadata
         return next()
+    } catch(err) {
+        return res.status(401).send({
+            "msg" : "access_token is required",
+            "error" : err
+        })
+    }
+}
+
+exports.auth_mentor = async (req, res, next) => {
+    try {
+        const user = jwt.verify(req.cookies.access_token, process.env.SUPABASE_JWT_KEY)
+        const { data } = await supabase
+            .from("pengguna")
+            .select("role")
+            .eq("auth", user.sub)
+
+        if (data[0].role != "biasa") {
+            req.user = user.user_metadata
+            console.log(data[0].role)
+            return next()
+        }
+        return res.status(403).send(
+            "<h1>403 Forbidden</h1>"
+        )
+
     } catch(err) {
         return res.status(401).send({
             "msg" : "access_token is required",
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { cache } = require("../middleware/cache");
+const auth_mentor = require("../middleware/auth").auth_mentor
 
 /* GET user info. */
-router.get('/', cache, function(req, res, next) {
+router.get('/', auth_mentor, cache, function(req, res, next) {
   res.send(req.user)
 });
 
