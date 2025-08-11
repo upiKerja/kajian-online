f1069e24d0e99613b0c61eca46b6a01d4e1e9f95
Kiru-I <julianbenhart@gmail.com>
2025-08-11 15:36:32 +0700

    added redis cache

--- routes/kajianRoutes.js
+++ routes/kajianRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const controller = require("../controller/kajianController")
+const { cache } = require("../middleware/cache");
 
-router.get('/select', controller.select)
-router.get('/cari', controller.cari)
-router.get('/carisemua', controller.carisemua)
+router.get('/select', cache, controller.select)
+router.get('/cari', cache, controller.cari)
+router.get('/carisemua', cache, controller.carisemua)
 router.post('/insert', controller.insert)
 router.delete('/delete/:id', controller.delete)
 
