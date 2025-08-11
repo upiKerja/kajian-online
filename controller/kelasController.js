52aed4cff0b0fe21eae11e2029a238874546c285
luthfi <kaplingtumbal@gmail.com>
2025-08-11 18:31:53 +0700

    Refactors controllers for modularity and improves error handling
    Standardizes table and column references across multiple controllers by introducing reusable variables for table names and IDs. Enhances error handling by adding detailed responses for empty or null data cases. Updates routes to include new endpoints for fetching and updating records. Improves maintainability and consistency of the codebase.

--- controller/kelasController.js
+++ controller/kelasController.js
@@ -1,71 +1,145 @@
 var exports = module.exports = {}
 var supabase = require("../database/supabase")
+var table = "kelas"
+var table_id = "id_" + table
 
-// Get all data
 exports.carisemua = async (req, res, next) => {
     const { data, error } = await supabase.client
-        .from("kelas")
-        .select("*")
+        .from(table)
+        .select("*, pemateri(nama_lengkap, foto_url, id_pemateri)")
 
-    if (data) {
-        res.status(200).send(data)
+    if (data === null || (Array.isArray(data) && data.length === 0)) {
+        return res.status(404).send({
+            message: "data tidak ditemukan",
+            status: "failed",
+            error: error
+        })
     }
+    return res.status(200).send({
+        message: "success",
+        status: "success",
+        data: data
+    })
 }
 
-// Get data by judul
 exports.cari = async (req, res, next) => {
     const { data, error } = await supabase.client
-        .from("kelas")
-        .select("*")
+        .from(table)
+        .select("*, pemateri(nama_lengkap, foto_url, id_pemateri)")
         .limit(10)
         .textSearch("judul", req.query.q, {
             type: "websearch",
             config: "english"
         })
 
-    if (data) {
-        res.status(200).send(data)
+    if (data === null || (Array.isArray(data) && data.length === 0)) {
+        return res.status(404).send({
+            message: "data tidak ditemukan",
+            status: "failed",
+            error: error
+        })
     }
+    return res.status(200).send({
+        message: "success",
+        status: "success",
+        data: data
+    })
 }
 
-// Get data by column
 exports.select = async (req, res) => {
     const { error, data } = await supabase.client
-    .from("kelas")
+    .from(table)
     .select("*")
     .eq(req.query.w, req.query.eq)
 
-    if (data) {
-        res.send(data)
-    } else {
-        res.send({error: error})
+    if (data === null || (Array.isArray(data) && data.length === 0)) {
+        return res.status(404).send({
+            message: "data tidak ditemukan",
+            status: "failed",
+            error: error
+        })
     }
+    return res.status(200).send({
+        message: "success",
+        status: "success",
+        data: data
+    })
 }
 
-// Post data
+exports.indexes = async (req, res) => {
+    const { data, error } = await supabase.client
+        .from(table)
+        .select("*, pemateri(nama_lengkap, foto_url, id_pemateri)")
+        .limit(1)
+        .eq("slug", req.params.slug)
+
+    if (data === null || (Array.isArray(data) && data.length === 0)) {
+        return res.status(404).send({
+            message: "data tidak ditemukan",
+            status: "failed",
+            error: error
+        })
+    }
+    return res.status(200).send({
+        message: "success",
+        status: "success",
+        data: data[0]
+    })
+}
+
+exports.update = async (req, res) => {
+    const { data, error } = await supabase.client
+        .from(table)
+        .update(req.body)
+        .eq(table_id, req.params.id)
+
+    if (!error) {
+        return res.status(200).send({
+            message: "success",
+            status: "success",
+            data: data
+        })
+    }
+    return res.status(404).send({
+        message: "data tidak ditemukan",
+        status: "failed",
+        error: error
+    })
+}
+
 exports.insert = async (req, res) => {
     const { data, error } = await supabase.client
-        .from("kelas")
+        .from(table)
         .insert([req.body])
         .single()
         .select("*")
 
     if (error) {
-        return res.status(400).send({ msg: "failed", error: error });
+        return res.status(400).send({
+            message: error.message,
+            status: "failed",
+            error: error
+        });
     }
     return res.status(201).send({ info: "success", data });
 }
 
-// Delete data by id
 exports.delete = async (req, res) => {
     const { data, error } = await supabase.client
-        .from("kelas")
+        .from(table)
         .delete()
-        .eq("id_kelas", req.params.id) // Delete row where id matches
+        .eq(table_id, req.params.id)
         .select("*")
 
     if (error) {
-        return res.status(400).send({ error: error.message });
+        return res.status(400).send({
+            message: error.message,
+            status: "failed",
+            error: error
+        });
     }
-    return res.status(200).send({ info: "success", data });
+    return res.status(200).send({
+        message: "success",
+        status: "success",
+        data : data});
 };
--- routes/kajianRoutes.js
+++ routes/kajianRoutes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const controller = require("../controller/kajianController")
 
+router.get('/index/:slug', controller.indexes)
 router.get('/select', controller.select)
 router.get('/cari', controller.cari)
 router.get('/carisemua', controller.carisemua)
 router.post('/insert', controller.insert)
+router.put('/update/:id', controller.update)
 router.delete('/delete/:id', controller.delete)
 
 module.exports = router;
