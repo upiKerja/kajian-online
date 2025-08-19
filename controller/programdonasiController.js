0569d5afdd55400b2de84443967145273356a1ea
Kiru-I <julianbenhart@gmail.com>
2025-08-19 12:35:10 +0700

    adding programdonasi to Controller

---
+++ controller/programdonasiController.js
@@ -0,0 +1,134 @@
+var exports = module.exports = {}
+var supabase = require("../database/supabase")
+var table = "program_donasi"
+var table_id = "id_" + table
+
+exports.carisemua = async (req, res, next) => {
+    const response = await supabase.client
+        .from(table)
+        .select("*, pengguna(nama_lengkap, foto_url)")
+
+    return res.status(response.status).send(response)
+}
+
+exports.discover = async (req, res) => {
+    const response = await supabase.client
+        .from(table)
+        .select("*, pengguna(nama_lengkap, foto_url)")
+        .eq("is_accepted", true)
+        .limit(req.query.limit || 20)
+
+    return res.status(response.status).send(response)
+}
+
+exports.accept = async(req, res) => {
+    const response = await supabase.client
+        .from(table)
+        .update({is_accepted: true})
+        .eq("id_kelas", req.params.id_kelas)
+        .single()
+
+    return res.status(response.status).send(response)    
+}
+
+exports.cari = async (req, res, next) => {
+    const response = await supabase.client
+        .from(table)
+        .select("judul, slug, pengguna(nama_lengkap, foto_url)")
+        .eq("is_accepted", true)
+        .limit(10)
+        .textSearch("judul", req.query.q, {
+            type: "websearch",
+            config: "english"
+        })
+
+    return res.status(response.status).send(response)
+}
+
+exports.select = async (req, res) => {
+    const response = await supabase.client
+        .from(table)
+        .select("*")
+        .eq(req.query.w, req.query.eq)
+
+    return res.status(response.status).send(response)
+}
+
+exports.indexes = async (req, res) => {
+    const response = await supabase.client
+        .from(table)
+        .select("*, pengguna(nama_lengkap, foto_url)")
+        .single()
+        .eq("slug", req.params.slug)
+
+    return res.status(response.status).send(response)
+}
+
+exports.sudoUpdate = async (req, res) => {
+    // Route Update khusus Admin
+    const response = await supabase.client
+        .from(table)
+        .update(req.body)
+        .eq(table_id, req.params.id_program_donasi)
+
+    return res.status(response.status).send(response)
+}
+
+exports.update = async (req, res) => {
+    // Route Update khusus authenticated mentor
+
+    // Jaga-jaga kalo mau ngupdate Judul
+    if (req.body.judul) {
+        req.body.slug = req.body.judul.replace(/[?&]/g, "").toLowerCase().trim().replaceAll(" ", "-")
+    } 
+
+    // Data yang gaboleh di Update
+    const {is_accepted, id_pengguna, id_program_donasi, ...inih} = req.body
+    req.body.is_accepted = false // Data yang udah diupdate harus di ACC lebih dulu ama Admin.
+    req.body = inih
+
+    const response = await supabase.client
+        .from(table)
+        .update(req.body)
+        .eq(table_id, req.params.id_kelas)
+
+    return res.status(response.status).send(response)
+}
+
+    exports.insert = async (req, res) => {
+        if (req.body.nama_program) {
+            req.body.slug = req.body.nama_program.replace(/[?&]/g, "").toLowerCase().trim().replaceAll(" ", "-")
+        } 
+        
+        const {is_accepted, ...inih} = req.body
+        req.body = inih
+        req.body.id_pengguna = req.internalUserId
+
+        const response = await supabase.client
+            .from(table)
+            .insert(req.body)
+            .select("*")
+
+        return res.status(response.status).send(response)
+    }
+
+exports.delete = async (req, res) => {
+    const response = await supabase.client
+        .from(table)
+        .delete()
+        .eq(table_id, req.params.id)
+        .select("*")
+
+    return res.status(response.status).send(response)
+};
+
+exports.daftar = async (req, res) => {
+    const response = await supabase.client
+        .from("log_kelas")
+        .insert({
+            id_pengguna: req.internalUserId,
+            id_kelas: req.params.id_kelas
+        })
+
+    return res.status(response.status).send(response)
+}
---
+++ routes/programdonasiRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const controller = require("../controller/programdonasiController")
+const { cache } = require("../middleware/cache");
+const { auth, auth_mentor, authenticated_mentor, auth_admin } = require("../middleware/auth")
+
+// Guess
+router.get('/index/:slug_kelas', cache, controller.indexes)
+router.get('/cari', cache, controller.cari)
+router.get('/discover', cache, controller.discover)
+
+// User
+router.post('/daftar/:id_kelas', auth, controller.daftar)
+
+// Admin
+router.get('/select', auth_admin, controller.select)
+router.get('/carisemua', auth_admin, controller.carisemua)
+router.put('/sudo/update/:id_kelas', auth_admin, controller.sudoUpdate)
+router.put('/accept/:id_kelas', auth_admin, controller.accept)
+router.delete('/delete/:id_kelas', auth_admin, controller.delete)
+
+// Mentor
+router.post('/insert', auth_mentor, controller.insert)
+
+// Authenticated Mentor
+router.put('/update/:id_kelas', auth_mentor, authenticated_mentor, controller.update)
+
+module.exports = router;
