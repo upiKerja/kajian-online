604a720ab8635c1d55036f7bf32e4079438fc6c8
upi-0 <upi@mis.my.id>
2026-01-20 13:50:27 +0700

    aing pengen beli teh cok

--- routes/kajianRoutes.js
+++ routes/kajianRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const controller = require("../controller/kajianController")
 const { cache, flush_cache } = require("../middleware/cache");
 const { auth_admin } = require("../middleware/auth")
+const { fileHandler, main_uploader } = require("../controller/fileuploadController");
+const { baseHandlingChange, baseAutoChange } = require("../middleware/file")
 
 // Guess
 router.get('/index/:slug_kajian', cache, controller.indexes)
@@ -17,5 +19,20 @@ router.get('/stats', auth_admin, cache, controller.stats)
 router.post('/insert', auth_admin, controller.insert, flush_cache)
 router.put('/update/:id_kajian', auth_admin, controller.update, flush_cache)
 router.delete('/delete/:id_kajian', auth_admin, controller.delete, flush_cache)
+
+router.post(
+    '/insert_with_thumbnail',
+    auth_admin,
+    main_uploader.single("thumbnail"),
+    baseHandlingChange("id_thumbnail_address"),
+    controller.insert
+)
+
+router.put(
+    '/:id_kajian/thumbnail/:id_file',
+    auth_admin,
+    fileHandler.single("thumbnail"),
+    baseAutoChange("kajian", "id_thumbnail_address", "id_kajian")
+)
 
 module.exports = router;
